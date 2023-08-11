217ea729cc2a49fb725c2d7ece65366f122337b2
Sêmpiternal <118236448+Sempiternal2k17@users.noreply.github.com>
2023-08-11 12:40:24 +0800

    Navbar
    Creating NavBar

---
+++ src/Components/Sempi/Sempi.js
@@ -0,0 +1,20 @@
+import '/.Sempi.css'
+
+function Sempi(){
+
+    const navLinks = [
+        'Characters',
+        'Artifacts',
+        'Regions'
+    ]
+
+    return (
+        <div className='links-sempi'>
+            <ul className='ul-sempi'>
+
+            </ul>
+        </div>
+    );
+}
+
+export default Sempi;
