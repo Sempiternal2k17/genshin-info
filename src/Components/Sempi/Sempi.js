ea4b71d1493fa1e75a425118f9121158d0239a9f
Sêmpiternal <118236448+Sempiternal2k17@users.noreply.github.com>
2023-08-11 19:36:06 +0800

    Fav Icon
    Change Fav Icon

--- src/Components/Sempi/Sempi.js
+++ src/Components/Sempi/Sempi.js
@@ -1,18 +1,30 @@
-import '/.Sempi.css'
+import './Sempi.css';
+import Logo from './Images/genshinLogo.png'
 
 function Sempi(){
 
     const navLinks = [
         'Characters',
         'Artifacts',
-        'Regions'
+        'Regions',
+        'Weapons'
     ]
 
     return (
-        <div className='links-sempi'>
-            <ul className='ul-sempi'>
 
-            </ul>
+        <div className='navbar'>
+            <div className='img-sempi'>
+                <img src={Logo} alt='Genshin Impact' />
+            </div>
+
+            <div className='links-sempi'>
+                <ul className='ul-sempi'>
+                    {navLinks.length === 0 && <h1>No items found</h1>}
+                    {navLinks.map(item =>
+                        <li className = "li-sempi" key={item}>{item}</li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 }
